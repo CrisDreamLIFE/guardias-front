c7706b06215b0fd123f3605b4e7904a48d253401
CrisDreamLIFE <cristian.sepulveda.co@usach.cl>
2024-08-03 18:55:30 -0400

    ADD init commit and bootstrap

---
+++ src/views/HomeView.vue
@@ -0,0 +1,33 @@
+<!-- <script setup>
+import TheWelcome from '../components/TheWelcome.vue'
+</script>
+
+<template>
+  <main>
+    <TheWelcome />
+  </main>
+</template> -->
+<template>
+  <div class="home">
+    <h1>Turno Manager</h1>
+    <nav>
+      <RouterLink to="/availability">Disponibilidad</RouterLink>
+      <RouterLink to="/turnos-confirmados">Turnos Confirmados</RouterLink>
+    </nav>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+.home {
+  text-align: center;
+}
+
+/* nav a {
+  margin: 0 10px;
+  text-decoration: none;
+  color: blue;
+} */
+</style>
